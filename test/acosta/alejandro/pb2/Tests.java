7e195dd3d24daa7e5898073c08aebf5121a07bfc
CobyBoy <aboutacoby@gmail.com>
2021-06-01 21:19:06 -0300

    added tests. one's broken

--- test/acosta/alejandro/pb2/Tests.java
+++ test/acosta/alejandro/pb2/Tests.java
@@ -50,5 +50,34 @@ public class Tests {
 		
 		assertEquals((Integer)0, biblioteca.prestarLibroAAlumno(1, alumno, libroDeMatematica));
 	}
+	
+	@Test
+	public void queUnAlumnoPuedaDevolverUnLibroYElLibroEsteHabilitadoAlDevolverlo() {
+		Biblioteca biblioteca = new Biblioteca("Biblioteca Unlam");
+		Libro libroDeMatematica = new LibroDeMatematica(1, "Matematicas Discretas", "unAutor");
+		biblioteca.agregarLibrosALaBiblioteca(libroDeMatematica);
+		biblioteca.agregarLibrosALaBiblioteca(new LibroDeGeografia(2, "Geografia mundial", "otroAutor"));
+		biblioteca.agregarLibrosALaBiblioteca(new LibroDeHistoria(3, "Historia Argentina", "tercerAutor"));
+		
+		Alumno alumno = new Alumno(35396973, "Coby", "Acosta");
+		
+		biblioteca.prestarLibroAAlumno(1, alumno, libroDeMatematica);
+		
+		//alumno.devolverLibro();
+		assertFalse(libroDeMatematica.getSePuedePrestar());
+	}
+	
+	@Test
+	public void queLosLibrosDeGeografiaSeanFotocopoiables() {
+		LibroDeGeografia geo = new LibroDeGeografia(2, "Geografia mundial", "otroAutor");
+		assertEquals("Geografia mundial", geo.libroQueSePuedeFotocopiar());
+	}
+	
+	@Test
+	public void queLosLibrosDeHistoriaSeanFotocopoiables() {
+		LibroDeHistoria historia = new LibroDeHistoria(3, "Historia Argentina", "tercerAutor");
+		assertEquals("Historia Argentina", historia.libroQueSePuedeFotocopiar());
+	}
+	
 
 }
